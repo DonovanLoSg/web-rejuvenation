f8663aaba3421b6c7040742deb58fcf52278b273
Donovan Lo <donovanlo1174@gmail.com>
2020-09-07 10:18:07 +0000

    Extract valid URL from a given string

--- test_BeautifulSoup.py
+++ test_BeautifulSoup.py
@@ -1,11 +1,33 @@
-# input string
-# test whether it is a valid URL
+import re  # import regular expressioon
+
+
+# this function will find and return all the valid url
+# in the given string and return them in an array.
+def find_valid_url(url_string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex, url_string)
+    return [x[0] for x in url]
+
+
+# define the starting url string.
+# this will be an input from the user
+starting_url = 'my homepage is http://resume.donovanlo.sg?all and linkedin profile is linkedin.sg'
+
+# place the found urls into urls array
+urls = find_valid_url(starting_url)
+print(urls)
+
+
 # extract domain name portion
 # extract domain name source directory
-# declare variables an array of urls
+# declare variables an array of objects urls
 # If it is a valid URL
 #     add string into array urls
 #     proceed with web crawling
+#     {add other required operation}
+#     word count // detemine pages
+#     multimedia file count
+#     images count
 #     for each links in url array
 #          get all the a links
 #          check whether it's an absolute url or relative url
