54e9116d28d8f9ae7e2943964b66ba2f0b5578e9
Donovan Lo <donovanlo1174@gmail.com>
2020-09-08 03:31:22 +0000

    Build absolute url

--- test_BeautifulSoup.py
+++ test_BeautifulSoup.py
@@ -1,6 +1,7 @@
-import re  # import regular expressioon
+# import regular expressioon
+import re
 import requests
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, urlparse, urljoin, ParseResult
 
 
 def find_valid_url(url_string):
@@ -14,8 +15,17 @@ def find_valid_url(url_string):
     url = re.findall(regex, url_string)
     return [x[0] for x in url]
 
+def is_valid_url(url_string):
+    """
+    check whether the given string is valid url using regex
+    param url_string: string
+    return type: boolean
+    """
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    return bool(re.search(regex, url_string))
 
-def get_base_url(url_string):
+
+def get_path(url_string):
     """
     split up the url_string to get the base url
     param url_string: string
@@ -24,10 +34,10 @@ def get_base_url(url_string):
     parts = urlsplit(url_string)
     base = "{0.netloc}".format(parts)
     strip_base = base.replace("www.", "")
-    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    starting_base_url = "{0.scheme}://{0.netloc}".format(parts)
     path = url_string[:url_string.rfind('/')+1] if '/' in parts.path else url_string
     print("strip_base: " + strip_base)
-    print("base_url:" + base_url)
+    print("starting_base_url:" + starting_base_url)
     print("path:" + path)
     return path
 
@@ -42,12 +52,40 @@ def is_alive(valid_url):
     return (r.status_code == 200)
 
 
+def build_absolute_url(url_string, base_url_string):
+    """
+    if the string is not a valid url with "http" or "https",
+    convert the string into one
+    return type: string (coverted url)
+    Reference: https://docs.python.org/3/library/urllib.parse.html
+    """
+    if is_valid_url(url_string):
+        p = urlparse(url_string, 'http')
+        netloc = p.netloc or p.path
+        path = p.path if p.netloc else ''
+        if not netloc.startswith('www.'):
+            netloc = 'www.' + netloc
+        p = ParseResult('http', netloc, path, *p[3:])
+        return p.geturl()
+    else:
+        return urljoin(base_url_string, url_string)
+
+
+
+
+
+
+
+
+
+
 """
 MAIN
 """
+
 # define the starting url string.
 # this will be an input from the user
-starting_string = 'my homepage is http://resume.donovanlo.sg?all and linkedin profile is www.linkedin.sg'
+starting_string = 'my homepage is http://resume.donovanlo.sg?all and linkedin profile is www.linkedin.sg http://www.example.com/some/path //www.example.com/some/path  /some/path'
 
 # place the found urls into urls array
 starting_urls = find_valid_url(starting_string)
@@ -56,13 +94,16 @@ url_list = []
 
 if len(starting_urls) > 0:
     # set the first url found as base url
-    base_url = starting_urls[0]
+    starting_base_url = starting_urls[0]
     # if the first url is reachable. start crawling
     if is_alive(starting_urls[0]):
         # add the starting url into the list as dict
+        # note: the starting url are retreived using find_valid_url,
+        # therefore, they will be valid url
         for each_url in starting_urls:
-            url_list.append({"url": each_url})
-            # check whether the url is a absolute url
+            url_list.append({"url": build_absolute_url(
+                each_url, starting_base_url)})
+
             # check whether the url belong to the same site as base url
             # if not a absolute url, prefix with base url
             # check whether the url is a valid url
@@ -70,15 +111,7 @@ if len(starting_urls) > 0:
             # 
 
         print(url_list)
-        print("-----------------")
-        print(get_base_url("http://resume.donovan.sg"))
-        print("-----------------")
-        print(get_base_url("https://resume.donovan.sg"))
-        print("-----------------")
-        print(get_base_url("resume.donovan.sg"))
-        print("-----------------")
-        print(get_base_url("/resume.donovan.sg"))
-        print("-----------------")
+
 
     else:
         print("The first url specified is unreachable.")
